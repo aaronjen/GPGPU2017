355ff37c21abec5bac4d6ebe61877e73345abc4d
bgarma <b01501009@ntu.edu.tw>
2017-04-28 15:46:55 +0800

    part 1 done

--- lab2/counting.cu
+++ lab2/counting.cu
@@ -10,8 +10,19 @@
 __device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
 __device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
 
+struct isAlpha {
+    __host__ __device__
+    int operator()(const char& x) const { return x != '\n'; }
+};
+
 void CountPosition1(const char *text, int *pos, int text_size)
 {
+    isAlpha op;
+    const thrust::device_ptr<const char> d_text = thrust::device_pointer_cast(text);
+    thrust::device_ptr<int> d_pos = thrust::device_pointer_cast(pos);
+
+    thrust::transform(thrust::device, d_text, d_text+text_size, d_pos, op);
+    thrust::inclusive_scan_by_key(thrust::device, d_pos, d_pos+text_size, d_pos, d_pos);
 }
 
 void CountPosition2(const char *text, int *pos, int text_size)
